9a47a93c7991c2a75fee680ce6d34dcb187b80a5
jihaneh <jijih@hotmail.fr>
2022-04-14 10:26:38 +0200

    Add reverse method

--- solver.rb
+++ solver.rb
@@ -1,9 +1,13 @@
 class Solver
-  def factorial(n)
-    raise 'number must not be negative' if n < 0
-    return 1 if n == 0
-    return 1 if n == 1
+  def factorial(number)
+    raise 'number must not be negative' if number < 0
+    return 1 if number == 0
+    return 1 if number == 1
 
-    (n * factorial(n - 1))
+    (number * factorial(number - 1))
+  end
+
+  def reverse(word)
+    word.reverse
   end
 end
