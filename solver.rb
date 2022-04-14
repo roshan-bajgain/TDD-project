26b8c22421a9b3f98092ccd1f1be9e5eaa575e98
jihaneh <jijih@hotmail.fr>
2022-04-14 10:35:26 +0200

    fix linters

--- solver.rb
+++ solver.rb
@@ -1,7 +1,7 @@
 class Solver
   def factorial(number)
-    raise 'number must not be negative' if number < 0
-    return 1 if number == 0
+    raise 'number must not be negative' if number.negative?
+    return 1 if number.zero?
     return 1 if number == 1
 
     (number * factorial(number - 1))
@@ -12,10 +12,10 @@ class Solver
   end
 
   def fizzbuzz(number)
-   return 'fizzbuzz' if number%3 == 0 && number%5 == 0
-   return 'fizz' if number%3 == 0
-   return 'buzz' if number%5 == 0
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
 
-   number.to_s
+    number.to_s
   end
 end
